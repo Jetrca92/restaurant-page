b9787888b25d27145d6a9e3ff75240ceac40b836
Jetrca92 <blaz.guzej@gmail.com>
2023-02-26 20:29:15 +0100

    Add js to buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello, juan');
+const btnNav = document.querySelectorAll('.button-nav');
+// Add active class if button clicked and remove it from the rest
+btnNav.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.classList.value === "button-nav active") return;
+        const oldButton = document.querySelector('.active');
+        oldButton.classList.remove('active');
+        button.classList.add('active');
+    });
+});
